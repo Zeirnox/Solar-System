8d7a3cd85bbeb4ef995047fe66e6dbcf7eb0aa76
Zeirnox <84430553+Zeirnox@users.noreply.github.com>
2022-02-05 10:28:26 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'https://cdn.skypack.dev/pin/three@v0.130.0-WI96Ec9p8dZb5AMcOcgD/mode=imports/optimized/three.js';
-import { OrbitControls } from "/orbitControls.js";
+import { OrbitControls } from "orbitControls.js";
 
 
 //setup
